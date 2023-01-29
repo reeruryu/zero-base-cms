398f325e344eab656c25cca998eb8df10b14c533
reeruryu <h2ju1004@gmail.com>
2023-01-29 18:47:25 +0900

    feat : [004-LOGIN-CUSTOMER] 커스터머에 대한 로그인 구현
    JWT의 내부 암호화
    /customer/* 에 대한 token filter 적용

---
+++ zerobase-domain/src/test/java/com/zerobase/cms/domain/util/Aes256UtilTest.java
@@ -0,0 +1,15 @@
+package com.zerobase.cms.domain.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Aes256UtilTest {
+
+	@Test
+	void encrypt() {
+		String encrypt = Aes256Util.encrypt("Hello world");
+		assertEquals(Aes256Util.decrypt(encrypt), "Hello world");
+	}
+
+}
